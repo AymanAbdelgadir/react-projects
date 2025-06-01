8ef0d3bb079bc7c01861bac1e9b90bb5c9fe775e
Ayman Abdelgadir <xxdayman@gmail.com>
2025-06-01 08:50:25 +0300

    Changing Readme file

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Counter from './componets/counter/Counter';
+const App = () => {
+    return (
+        <div>
+           < Counter />
+        </div>
+    );
+};
+
+export default App;
--- src/componets/counter/Counter.jsx
+++ src/componets/counter/Counter.jsx
@@ -1,2 +1,27 @@
 import {useState} from 'react';
-import '../style.css'
+import  '../../style.css'
+import React from 'react';
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+    const increment = () => {
+        setCount(count + 1);
+    }
+    const decrement = () => {
+        setCount(count - 1);
+    }
+    return (
+        <div className="container">
+            <div>
+                <h1 className='number'>{count}</h1>
+            </div>
+            <div className='btns-container'>
+                <button onClick={increment} className='btn'>  +  </button>
+                <button onClick={decrement} className='btn'>  -  </button>
+
+            </div>
+        </div>
+    );
+};
+
+export default Counter;
